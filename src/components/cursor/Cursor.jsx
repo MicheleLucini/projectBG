26db79f4732e393e8b426aae26febe1dae095317
Lucini Michele <mic.lucini@gmail.com>
2021-06-02 01:25:50 +0200

    cursor click animation

--- src/components/cursor/Cursor.jsx
+++ src/components/cursor/Cursor.jsx
@@ -5,11 +5,18 @@ import "./cursor.css";
 
 const Cursor = ({}) => {
   const [hide, setHide] = useState(false);
+  const [mouseDown, setMouseDown] = useState(false);
+  const [mouseUp, setMouseUp] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const cursorClass = useMemo(
-    () => [hide ? "hidden" : null].filter((x) => !!x),
-    [hide]
+    () =>
+      [
+        hide ? "hidden" : null,
+        mouseDown ? "down" : null,
+        mouseUp ? "up" : null,
+      ].filter((x) => !!x),
+    [hide, mouseDown, mouseUp]
   );
 
   const handleMouseMove = useCallback((event) => {
@@ -20,11 +27,21 @@ const Cursor = ({}) => {
 
   const handleMouseEnter = useCallback(() => setHide(false), []);
   const handleMouseLeave = useCallback(() => setHide(true), []);
+  const handleMouseDown = useCallback(() => {
+    setMouseUp(false);
+    setMouseDown(true);
+  }, []);
+  const handleMouseUp = useCallback(() => {
+    setMouseDown(false);
+    setMouseUp(true);
+  }, []);
 
   useEffect(() => {
     document.onmousemove = handleMouseMove;
     document.onmouseenter = handleMouseEnter;
     document.onmouseleave = handleMouseLeave;
+    document.onmousedown = handleMouseDown;
+    document.onmouseup = handleMouseUp;
   });
 
   return (
