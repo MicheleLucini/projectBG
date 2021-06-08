2c86d48f9e05fa19bb733a7edc07fbd0f9ff6a0a
Lucini Michele <mic.lucini@gmail.com>
2021-06-08 22:49:05 +0200

    Menu

--- src/scenes/menu/Menu.jsx
+++ src/scenes/menu/Menu.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Button from "../../components/button";
+import TextInput from "../../components/textInput";
 import { GAME_PHASES } from "../../logic/constants";
 
 import "./menu.css";
 
 const Menu = ({ changePhase }) => {
+  const [name, setName] = useState("");
+
   return (
     <div id="menu">
+      <TextInput label="Name" value={name} setValue={setName} />
       <Button
         text="Start campaign"
         icon="play_arrow"
         onClick={() => changePhase(GAME_PHASES.LOBBY_PREGAME)}
+        disabled={name.length === 0}
       ></Button>
     </div>
   );
