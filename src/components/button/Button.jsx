39f82e2d6d2a59703c31902c8a0628e1e2f7dfe4
Lucini Michele <mic.lucini@gmail.com>
2021-06-08 22:48:46 +0200

    Button

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,9 +3,13 @@ import PropTypes from "prop-types";
 
 import "./button.css";
 
-const Button = ({ text, icon, onClick }) => {
+const Button = ({ text, icon, onClick, disabled }) => {
   return (
-    <button className="simple-button" type="button" onClick={onClick}>
+    <button
+      className={"simple-button" + (disabled ? " disabled" : "")}
+      type="button"
+      onClick={onClick}
+    >
       {icon && <span className="material-icons-round">{icon}</span>}
       {text && text}
     </button>
@@ -16,12 +20,14 @@ Button.propTypes = {
   text: PropTypes.string,
   icon: PropTypes.string,
   onClick: PropTypes.func,
+  disabled: PropTypes.bool,
 };
 
 Button.defaultProps = {
   text: null,
   icon: null,
   onClick: () => {},
+  disabled: false,
 };
 
 export default Button;
